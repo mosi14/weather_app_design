40b9a3370575d3c04b25fb469bb7ee781c3591ee
mosio14 <mostafa.asadollahy@gmail.com>
2024-12-12 17:05:07 +0100

    add sample of time and

--- src/components/TimeAndLocation.jsx
+++ src/components/TimeAndLocation.jsx
@@ -1,8 +1,16 @@
-
 export default function TimeAndLocation() {
   return (
     <div>
-      
+      <div className="flex flex-row justify-center my-6">
+        <p className="text-xl font-extralight">
+          Tuesday, 14 May 2024 | local time: 7: 04 AM
+        </p>
+      </div>
+      <div className="flex flex-row justify-center my-3">
+        <p className="text-xl font-extralight">
+          Torino, TO
+        </p>
+      </div>
     </div>
-  )
+  );
 }
