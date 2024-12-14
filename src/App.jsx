5cd8ec94521b9076a18e0ead12cade208a56c6f8
mosio14 <mostafa.asadollahy@gmail.com>
2024-12-14 19:22:26 +0100

    change background base on temp

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,24 @@ function App() {
     getWeather();
   }, [query, units]);
 
+  const formatBackground = () => {
+    if (!weather) return "from-cyan-600 to-blue-700";
+    const threshold = units === "metric" ? 20 : 60;
+    if (weather.temp <= threshold) return "from-cyan-600 to-blue-700";
+    return "from-yellow-600 to-orange-700";
+  };
+
   return (
-    <div className="mx-auto max-w-screen-lg mt-4 py-5 px-32 bg-gradient-to-br shadow-xl shadow-gray-500 from-cyan-600 to-blue-700">
+    <div
+      className={`mx-auto max-w-screen-lg mt-4 py-5 px-32 bg-gradient-to-br shadow-xl shadow-gray-400  ${formatBackground()}`}
+    >
       <TopButtons setQuery={setQuery} />
-      <Input />
+      <Input setQuery={setQuery} setUnits={setUnits} />
 
       {weather && (
         <>
           <TimeAndLocation weather={weather} />
-          <TempAndDetails weather={weather} />
+          <TempAndDetails weather={weather} units={units}/>
           <Forecast title="3 hour step forecast" data={weather.hourly} />
           <Forecast title="daily forecast" data={weather.daily} />
         </>
